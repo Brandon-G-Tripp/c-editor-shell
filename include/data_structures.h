04d5957a8a7758f25e8a62a6928aaad1583f12f3
Brandon Tripp <brandongtripp@gmail.com>
2024-01-24 15:16:29 -0700

    Write header file for our basic data structures

---
+++ include/data_structures.h
@@ -0,0 +1,33 @@
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
+
+// Generic list type 
+typedef struct List List;
+
+// Generic vector type 
+typedef struct Vector Vector ;
+
+// Generic hash map type 
+typedef struct Map Map ;
+
+// List functions
+List* initList();
+void destroyList(List* list);
+void insertList(List* list, void* element);
+void* getList(List* list, int index);
+
+
+// Vector functions
+Vector* initVector();
+void destroyVector(Vector* vector);
+void insertVector(Vector* vector, void* element);
+void* getVector(Vector* vector, int index);
+
+
+// Map functions
+Map* initMap(int capacity);
+void destroyMap(Map* map);
+void insertMap(Map* map, void* key, void* value);
+void* getMap(Map* map, void* key);
+
+#endif
