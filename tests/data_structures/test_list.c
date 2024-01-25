5da5f20a4ee9074a49db25b306157596a9d8c42d
Brandon Tripp <brandongtripp@gmail.com>
2024-01-25 12:12:28 -0700

    Correct of the path for list.h

--- tests/data_structures/test_list.c
+++ tests/data_structures/test_list.c
@@ -1,5 +1,37 @@
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include <stddef.h> 
 #include <setjmp.h>
-#include <cmocka.h>
-#include "data_structures.h"
+/* #include <cmocka.h> */
+#include "../../include/data_structures.h"
+
+static void setup() {  
+}
+
+static void teardown() {
+}
+
+void test_sample() {
+    // Arrange 
+    setup();
+
+    // Act 
+
+    /* Assert */
+    bool success = true;
+
+    if (!success) {
+        printf("Sample test failed!\n");
+    } else {
+        printf("Sample test passed!\n");
+    } 
+
+    teardown();
+}
+
+int main(void) { 
+    test_sample();
+
+    return 0;
+}
