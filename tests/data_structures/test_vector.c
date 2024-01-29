c890e90a0153ca8ffbb1ce0bb84e81550d66cf58
Brandon Tripp <brandongtripp@gmail.com>
2024-01-29 16:26:42 -0700

    Basic tests for the vector data structure that test all of the function declarations we have so far in the header file

--- tests/data_structures/test_vector.c
+++ tests/data_structures/test_vector.c
@@ -4,7 +4,47 @@
 #include <stdbool.h>
 #include <stddef.h> 
 #include <setjmp.h>
-#include "../../cmake/cmocka/include/cmocka.h"
+#include <stdlib.h>
+/* #include "../../cmake/cmocka/include/cmocka.h" */
+#include "/usr/local/Cellar/cmocka/1.1.7/include/cmocka.h"
 
+#include "../../include/data_structures.h"
+#include "../../src/data_structures/vector.c"
 
+static void test_init_vector() {
+    Vector* vector = initVector();
+    assert_non_null(vector);
+} 
 
+static void test_destroy_vector() {
+    Vector* vector = initVector();
+    destroyVector(vector);
+} 
+
+static void test_insert_vector() {
+    Vector* vector = initVector();
+    int element = 10;
+    insertVector(vector, &element);
+    assert_int_equal(*(int *)getVector(vector, 0), 10);
+    destroyVector(vector);
+} 
+
+static void test_get_vector() {
+    Vector* vector = initVector();
+    int element = 20;
+    insertVector(vector, &element);
+    int* result = getVector(vector, 0);
+    assert_int_equal(*result, 20);
+    destroyVector(vector);
+} 
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_init_vector),
+        cmocka_unit_test(test_destroy_vector),
+        cmocka_unit_test(test_insert_vector),
+        cmocka_unit_test(test_get_vector),
+    }; 
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+} 
