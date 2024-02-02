ecc0c73dd7a614f38e732ca1cabda124b17bf35d
Brandon Tripp <brandongtripp@gmail.com>
2024-02-02 14:27:39 -0700

    Improve count test to test functionality

--- tests/utils/test_common_utils.c
+++ tests/utils/test_common_utils.c
@@ -1,4 +1,6 @@
 #include "../../include/utils/common_utils.h"
+#include "../../src/utils/common_utils.c"
+
 
 #include <stdarg.h>
 #include <stddef.h>
@@ -7,20 +9,18 @@
 #include "/usr/local/Cellar/cmocka/1.1.7/include/cmocka.h"
 
 // Test count 
-void test_count(void **state) {
-    char* arr[] = {"a", "b", "c"};
+void test_count_string(void **state) {
+    char *arr[] = {"a", "b", "c", NULL};
 
-    int len = count(arr);
+    int len = count_string(arr);
 
     assert_int_equal(len, 3);
 } 
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_count),
+        cmocka_unit_test(test_count_string),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
-
-
 } 
