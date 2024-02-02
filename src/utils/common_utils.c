d16cf3783474df8ca38b01630d3f940dba25255c
Brandon Tripp <brandongtripp@gmail.com>
2024-02-02 14:28:28 -0700

    Refactor of the count function in common utils

--- src/utils/common_utils.c
+++ src/utils/common_utils.c
@@ -1,12 +1,14 @@
 #include "../../include/utils/common_utils.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 // Get length of char array
-int count(char** arr) {
-    int len = 0;
-    while(arr[len] != NULL) {
-        len++;
+int count_string(char** arr) {
+    int i = 0;
+    while(arr[i] != NULL) {
+        printf("arr[%d]: %s\n", i, arr[i]); 
+        i++;
     } 
 
-    return len;
+    return i;
 } 
